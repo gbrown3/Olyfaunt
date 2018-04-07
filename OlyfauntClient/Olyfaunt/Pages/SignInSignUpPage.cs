2a4f2fc3b9a7e2de3c6ad166daa66def896c41eb
Gabe <gbrown3@macalester.edu>
2018-04-07 12:49:42 -0500

    blank sign in/up page added

---
+++ OlyfauntClient/Olyfaunt/Pages/SignInSignUpPage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace Olyfaunt.Pages
+{
+    public class SignInSignUpPage : ContentPage
+    {
+        public SignInSignUpPage()
+        {
+            Content = new StackLayout
+            {
+                Children = {
+                    new Label { Text = "Hello ContentPage" }
+                }
+            };
+        }
+    }
+}
+
