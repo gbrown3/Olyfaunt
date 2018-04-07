db8f83b80e2dc625b09042fd4b0bdc4b3683f8d7
Gabe <gbrown3@macalester.edu>
2018-04-07 12:58:24 -0500

    Added outline of user UI element

---
+++ OlyfauntClient/Olyfaunt/UI Elements/UserUIElement.cs
@@ -0,0 +1,37 @@
+﻿using System;
+namespace Olyfaunt.UIElements
+{
+    /// <summary>
+    /// UI element to use when looking at lists of users
+    /// </summary>
+    public class UserUIElement
+    {
+        private string displayName;    // internal storage of displayName
+        private string username;
+
+        public string DisplayName 
+        {
+            get 
+            {
+                return this.displayName;
+            }
+        }
+        public string Username
+        {
+            get
+            {
+                return this.username;
+            }
+        }
+
+
+        public UserUIElement(string username)
+        {
+            this.username = username;
+
+            // Get info from server about display name
+
+            // Get info from server about profile
+        }
+    }
+}
