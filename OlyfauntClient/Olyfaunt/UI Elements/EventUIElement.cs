a4c228b3394fa09ebbeca30c44d3c5f328d506b2
Gabe <gbrown3@macalester.edu>
2018-04-07 15:29:35 -0500

    Some work for events

---
+++ OlyfauntClient/Olyfaunt/UI Elements/EventUIElement.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Xamarin.Forms;
+namespace Olyfaunt
+{
+    public class EventUIElement : Grid
+    {
+        
+        public EventUIElement(Event e)
+        {
+            // Initialize layout
+            RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
+            RowDefinitions.Add(new RowDefinition { Height = new GridLength(3, GridUnitType.Star) });
+            ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) });
+            ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) });
+
+            // Make things to go in grid
+            Label titleLabel = new Label { Text = e.Name };
+
+        }
+    }
+}
---
+++ OlyfauntClient/Olyfaunt/UI Elements/ProblemUIElement.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Xamarin.Forms;
+namespace Olyfaunt.UIElements
+{
+    /// <summary>
+    /// UI Element to display a problem, as well as ways to react to it 
+    /// </summary>
+    public class ProblemUIElement : Grid
+    {
+        public ProblemUIElement()
+        {
+            // Initialize layout
+            //VerticalOptions = new LayoutOptions
+
+            //RowDefinitions.Add(new RowDefinition { Height = new GridLength()});
+        }
+    }
+}
