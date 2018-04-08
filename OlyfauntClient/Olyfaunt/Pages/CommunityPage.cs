80b31c8c2579884e6cc30bcb61b487bd6821e1fc
Ansel Colby <dcolby1@macalester.edu>
2018-04-07 22:49:16 -0500

    Community page (not working)
    ahh

---
+++ OlyfauntClient/Olyfaunt/Pages/CommunityPage.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace Olyfaunt
+{
+    public class CommunityPage : ContentPage
+    {
+
+        StackLayout stack;
+        ScrollView scroller;
+
+        public CommunityPage()
+        {
+            Title = "My Community";
+
+            stack = new StackLayout();
+            scroller = new ScrollView();
+
+            Padding = new Thickness(5, 5);
+
+            for (int i = 0; i < 40; i++)
+            {
+                stack.Children.Add(new EventUIElement(new Event("Event" + i, "Sick event bro", "April 20", "4:20 pm")));
+            }
+
+            scroller.Content = stack;
+            Content = scroller;
+        }
+    }
+}
